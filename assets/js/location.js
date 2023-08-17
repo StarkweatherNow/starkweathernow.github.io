768014ad00bf8af9459db397f6c7b6b6e1dcedef
StarkweatherNow <starkweather@me.com>
2023-08-17 15:24:02 -0400

    added weather JS widget to gym page

---
+++ assets/js/location.js
@@ -0,0 +1,19 @@
+//Request current user GPS from browser with error handling
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+}
+  
+function successFunction(position) {
+    console.log(position);
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    console.log(lat);
+    console.log(long);
+    getWeather(lat,long);
+}
+  
+function errorFunction() {
+    console.log("Unable to retrieve your location.");
+}
