14e2d145008a7629c3072b7902e94c57dccdb0e6
StarkweatherNow <starkweather@me.com>
2023-09-01 15:09:12 -0400

    working on JS to display JSON

--- pages/js/qotd.js
+++ pages/js/qotd.js
@@ -1,30 +1,22 @@
 //Data Source for Dashboard Widget - Quote of the Day
-//Data is imported via JSON file - qotd.json
+//import JSON file - qotd.json
+import data from ('./quotes.json') assert { type: "json" };
 
-//Create the HTML elements
-var widget = document.createElement('div');
-widget.className = 'widget';
-var widgetHeader = document.createElement('div');
-widgetHeader.className = 'widget-header';
-var widgetBody = document.createElement('div');
-widgetBody.className = 'widget-body';
-var widgetTitle = document.createElement('div');
-widgetTitle.className = 'widget-title';
-var widgetButton = document.createElement('div');
-widgetButton.className = 'widget-button';
-var widgetContent = document.createElement('div');
-widgetContent.className = 'widget-content';
+//Get the data from the JSON file
+var quote = data.quote;
+var author = data.author;
 
-//Populate the HTML elements with the data
-widgetTitle.innerHTML = 'Quote of the Day';
-widgetContent.innerHTML = quote + '<br><br>' + author;
+//parse data into a string
+quote = JSON.stringify(quote);
+author = JSON.stringify(author);
 
-//Append the HTML elements to the widget
-widgetHeader.appendChild(widgetTitle);
-widgetHeader.appendChild(widgetButton);
-widgetBody.appendChild(widgetContent);
-widget.appendChild(widgetHeader);
-widget.appendChild(widgetBody);
+//remove the quotation marks from the string
+quote = quote.replace(/\"/g, "");
+author = author.replace(/\"/g, "");
 
-//Append the widget to the dashboard
-document.getElementById('dashboard').appendChild(widget);
+//append quote to ID qotdquote
+document.getElementById("qotdQuote").innerHTML = quote;
+
+//append author to ID qotdauthor
+document.getElementById("qotdAuthor").innerHTML = author;
+
