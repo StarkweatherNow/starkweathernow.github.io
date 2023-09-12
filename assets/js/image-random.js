acb62b29c7e003bf9fc0b91492840f5761700269
StarkweatherNow <starkweather@me.com>
2023-09-12 10:52:17 -0400

    trying to convert base64 result

--- assets/js/image-random.js
+++ assets/js/image-random.js
@@ -6,9 +6,9 @@ $.ajax({
     headers: { 'X-Api-Key': 'HKbdvSJTDOWdxNPGQtd3wA==N9x9ZynA8zBsTFLA', 'Accept': 'image/jpg'},
     success: function(result) {
         console.log(result);
-        //Parse Array from result
-        var result = result[0];
-        $('#image-content').html(result.image);
+        // Convert base64-encoded image in JPEG format to html
+        result = '<img src="data:image/jpg;base64,' + result + '">';
+        $('#image-content').html(result);
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
