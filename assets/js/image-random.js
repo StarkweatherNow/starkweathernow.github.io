703cf0acf6104d6a7411d78076d81d7d5b74c76b
StarkweatherNow <starkweather@me.com>
2023-09-12 11:11:19 -0400

    decoding base64 to string

--- assets/js/image-random.js
+++ assets/js/image-random.js
@@ -5,8 +5,11 @@ $.ajax({
     url: 'https://api.api-ninjas.com/v1/randomimage?category=' + category,
     headers: { 'X-Api-Key': 'HKbdvSJTDOWdxNPGQtd3wA==N9x9ZynA8zBsTFLA', 'Accept': 'image/jpg'},
     success: function(result) {
+        //Decode Base64 to string
+        result = atob(result);
+        
         console.log(result);
-        // Convert base64-encoded image in JPEG format to html
+        
         result = '<img src="data:image/jpg;base64,/9j/' + result + '">';
         document.getElementById('image-content').innerHTML = result;
     },
