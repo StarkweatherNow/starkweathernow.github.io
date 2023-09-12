86c449c819bcaa302bd9a04d74443fec36197f9b
StarkweatherNow <starkweather@me.com>
2023-09-12 09:14:30 -0400

    random image generator added to test.html

---
+++ assets/js/image-random.js
@@ -0,0 +1,14 @@
+var data = 'https://api-ninjas.com';
+var category = 'technology'
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/randomimage?category=' + category,
+    headers: { 'X-Api-Key': 'HKbdvSJTDOWdxNPGQtd3wA==N9x9ZynA8zBsTFLA', 'Accept': 'image/jpg'},
+    success: function(result) {
+        //console.log(result);
+        $('#random-image').html(result.image);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
