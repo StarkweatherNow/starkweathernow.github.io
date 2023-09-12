9093a044d80557979bb51beaa1b00557bff61c68
StarkweatherNow <starkweather@me.com>
2023-09-12 10:27:49 -0400

    revert to working code

--- assets/js/qotd.js
+++ assets/js/qotd.js
@@ -8,16 +8,8 @@ $.ajax({
     contentType: 'application/json',
     success: function(result) {
         //console.log(result);
-        //parse result into array
-        var quote = JSON.parse(result);
-        console.log(quote);
-        console.log(result.quote);
-        console.log(result.author);
-
-        document.getElementById('quote-content').innerHTML = quote.quote;
-        document.getElementById('quote-author').innerHTML = quote.author;
-        //$('#quote-content').html(result.quote);
-        //$('#quote-author').html(result.author);
+        $('#quote-content').html(result.quote);
+        $('#quote-author').html(result.author);
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
