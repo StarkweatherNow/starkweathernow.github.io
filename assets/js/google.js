6c8ac421dc5faef8f0ca93712202b8b23843959a
StarkweatherNow <starkweather@me.com>
2023-09-12 11:55:19 -0400

    adding Google Cal quickstart code

--- assets/js/google.js
+++ assets/js/google.js
@@ -133,3 +133,38 @@ async function fetchTaskLists() {
 }
 
 //Google Calendar "Quick Start" code
+
+/**
+       * Print the summary and start datetime/date of the next ten events in
+       * the authorized user's calendar. If no events are found an
+       * appropriate message is printed.
+       */
+
+async function listUpcomingEvents() {
+    let response;
+    try {
+      const request = {
+        'calendarId': 'primary',
+        'timeMin': (new Date()).toISOString(),
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 10,
+        'orderBy': 'startTime',
+      };
+      response = await gapi.client.calendar.events.list(request);
+    } catch (err) {
+      document.getElementById('google_calendar_content').innerText = err.message;
+      return;
+    }
+
+    const events = response.result.items;
+    if (!events || events.length == 0) {
+      document.getElementById('google_calendar_content').innerText = 'No events found.';
+      return;
+    }
+    // Flatten to string to display
+    const output = events.reduce(
+        (str, event) => `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
+        'Events:\n');
+    document.getElementById('google_calendar_content').innerText = output;
+  }
