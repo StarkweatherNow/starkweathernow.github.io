f2f7a46d46dce9f6fd0c23eb014481f84c366a49
StarkweatherNow <starkweather@me.com>
2023-09-13 12:46:25 -0400

    removed refs to google auth button

--- assets/js/google.js
+++ assets/js/google.js
@@ -13,16 +13,7 @@ const API_KEY = 'AIzaSyCMsEWCT7HVaedHBid5YJ6ehFWJQZ4xf3U';
 // Discovery doc URL for APIs used by the quickstart
 const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest';
 
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-const SCOPES = 'https://www.googleapis.com/auth/tasks.readonly';
 
-let tokenClient;
-let gapiInited = false;
-let gisInited = false;
-
-document.getElementById('authorize_button').style.visibility = 'hidden';
-document.getElementById('signout_button').style.visibility = 'hidden';
 
 /**
  * Callback after api.js is loaded.
@@ -41,7 +32,6 @@ document.getElementById('signout_button').style.visibility = 'hidden';
             discoveryDocs: [DISCOVERY_DOC],
         });
         gapiInited = true;
-        maybeEnableButtons();
     }
 
 /**
@@ -54,54 +44,9 @@ document.getElementById('signout_button').style.visibility = 'hidden';
             callback: '', // defined later
         });
         gisInited = true;
-        maybeEnableButtons();
     }
 
-/**
- * Enables user interaction after all libraries are loaded.
- */
-    function maybeEnableButtons() {
-        if (gapiInited && gisInited) {
-            document.getElementById('authorize_button').style.visibility = 'visible';
-        }
-    }
 
-/**
- *  Sign in the user upon button click.
- */
-    function handleAuthClick() {
-        tokenClient.callback = async (resp) => {
-            if (resp.error !== undefined) {
-            throw (resp);
-            }
-            document.getElementById('signout_button').style.visibility = 'visible';
-            document.getElementById('authorize_button').innerText = 'Refresh';
-            await fetchTaskLists();
-        };
-
-        if (gapi.client.getToken() === null) {
-            // Prompt the user to select a Google Account and ask for consent to share their data
-            // when establishing a new session.
-            tokenClient.requestAccessToken({prompt: 'consent'});
-        } else {
-            // Skip display of account chooser and consent dialog for an existing session.
-            tokenClient.requestAccessToken({prompt: ''});
-        }
-    }
-
-/**
- *  Sign out the user upon button click.
- */
-    function handleSignoutClick() {
-        const token = gapi.client.getToken();
-        if (token !== null) {
-            google.accounts.oauth2.revoke(token.access_token);
-            gapi.client.setToken('');
-            document.getElementById('google_tasks_content').innerText = '';
-            document.getElementById('authorize_button').innerText = 'Authorize';
-            document.getElementById('signout_button').style.visibility = 'hidden';
-        }
-    }
 
 /**
  * Print task lists.
