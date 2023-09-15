d785c44d1c911d94043ae3373133a453f666a0b1
StarkweatherNow <starkweather@me.com>
2023-09-15 10:18:29 -0400

    added missing callback functions

--- assets/js/google.js
+++ assets/js/google.js
@@ -1,1 +1,25 @@
 //Collection of JS functions for Google Tasks + Calendar API
+
+//gapiLoaded is a function that runs when the Google API is loaded
+function gapiLoaded() {
+    //Load the Google API Client Library for JavaScript
+    gapi.load('client', initClient);
+}
+
+//gisLoaded is a function that runs when the Google API is loaded
+function gisLoaded() {
+    //Load the Google API Client Library for JavaScript
+    gapi.load('client', initClient);
+}
+
+//Function for handleauthclick
+function handleAuthClick(event) {
+    //Sign in the user upon button click.
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+//Function for handlesignoutclick
+function handleSignoutClick(event) {
+    //Sign out the user upon button click.
+    gapi.auth2.getAuthInstance().signOut();
+}
